004c1d5052f3f8e59e93389d8613af87bcf57952
Eray Sahin <eraysahinmoon@gmail.com>
2023-09-22 10:51:26 -0400

    Added Back Account Link

--- components/Header.js
+++ components/Header.js
@@ -76,6 +76,7 @@ const Header = () => {
             <NavLink href={'/'}>Home</NavLink>
             <NavLink href={'/products'}>All products</NavLink>
             <NavLink href={'/categories'}>Categories</NavLink>
+            <NavLink href={'/account'}>Account</NavLink>
             <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
           </StyledNav>
           <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
