d07e05f39fde9a9235c98f1cffcadd2f2500294f
Eray Sahin <eraysahinmoon@gmail.com>
2023-09-26 23:02:24 -0400

    added secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_FRONT_ID,
